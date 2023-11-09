65c48ef19de7d2f00d37c1a2ebcf625cc0383016
Gubanova Valeria <gubanovavaleria07@gmail.com>
2023-11-09 23:46:25 +0600

    Добавлен комментарий пользователя Отрефакторила код, теперь всё в отдельных функциях, цвета вынесены в файл Color.kt

--- app/src/main/java/com/example/app3/MainActivity.kt
+++ app/src/main/java/com/example/app3/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SliderColors
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -43,7 +44,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
+import com.example.app3.ui.theme.Background
+import com.example.app3.ui.theme.TextColor
 import com.example.app3.ui.theme.Yellow
+
 
 
 class MainActivity : ComponentActivity() {
@@ -54,7 +58,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = colorResource(id = R.color.black)
+                    color = Background
                 ) {
                     Screen("Android")
                 }
@@ -68,87 +72,166 @@ class MainActivity : ComponentActivity() {
 fun Screen(name: String, modifier: Modifier = Modifier) {
     LazyColumn(modifier = Modifier.fillMaxHeight()) {
       item {
-          Image(
-              painter = painterResource(id = R.drawable.img), contentDescription = null,
-              modifier = modifier
-                  .fillMaxWidth(), contentScale = ContentScale.FillWidth
-          )
+          StartImage(modifier)
       }
         item {
-            Box(
-                modifier = Modifier.offset(15.dp, (-50).dp)
-                    .size(150.dp)
-                    .clip(RoundedCornerShape(13.54.dp)),
-                contentAlignment = Alignment.Center
-
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.img_2), contentDescription = null,
-                    modifier = Modifier.size(150.dp, 150.dp)
-                        .align(Alignment.Center)
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.img_1), contentDescription = null,
-                    modifier = Modifier.size(60.dp)
-                        .offset((-6).dp, (-12).dp),
-                    alignment = Alignment.Center
-                )
-            }
+           LogoGame()
         }
         item {
-            Text(
-                color = Color(238, 242, 251, 70),
-                text = "Dota 2 is a multiplayer online battle arena (MOBA) " +
-                        "game which has two teams of five players compete to collectively destroy a large " +
-                        "structure defended by the opposing team known as the \"Ancient\", whilst defending their own.!",
-                modifier = modifier
-                    .size(330.dp)
-                    .offset((40).dp, (-10).dp)
-            )
+            DescriptionGame(modifier)
         }
         item {
-            Row(
-                modifier = Modifier.fillMaxWidth()
-                    .offset(40.dp, (-130).dp)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.screen1),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(240.dp, 135.dp)
-                        .clip(RoundedCornerShape(13.54.dp))
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.screen2),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(140.dp, 131.dp)
-                        .clip(RoundedCornerShape(35.64.dp))
-
-                )
-                }
+            SliderVideo()
             }
+        item {
+            DescriptionComment()
+        }
+        item {
+            Comment()
+        }
             item {
-                Box(
-                    modifier = Modifier .offset(40.dp, (-50).dp)
-                        .width(327.dp)
-                        .height(64.dp).clip(RoundedCornerShape(13.54.dp))
-                        .background(Yellow)
-                ) {
-                    Text(
-                        text = "Install",
-                        color = Color(0xFF050B18),
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.fillMaxSize()
-                            .offset(0.dp, 20.dp)
-                    )
-                }
+                ButtonInstall()
             }
-
     }
 
 }
 
+@Composable
+fun StartImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.img), contentDescription = null,
+        modifier = modifier
+            .fillMaxWidth(), contentScale = ContentScale.FillWidth
+    )
+}
+@Composable
+fun LogoGame() {
+    Box(
+        modifier = Modifier
+            .offset(15.dp, (-50).dp)
+            .size(150.dp)
+            .clip(RoundedCornerShape(13.54.dp)),
+        contentAlignment = Alignment.Center
+
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_2), contentDescription = null,
+            modifier = Modifier
+                .size(150.dp, 150.dp)
+                .align(Alignment.Center)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.img_1), contentDescription = null,
+            modifier = Modifier
+                .size(60.dp)
+                .offset((-6).dp, (-12).dp),
+            alignment = Alignment.Center
+        )
+    }
+}
+@Composable
+fun DescriptionGame(modifier: Modifier = Modifier) {
+    Text(
+        color = TextColor,
+        text = "Dota 2 is a multiplayer online battle arena (MOBA) " +
+                "game which has two teams of five players compete to collectively destroy a large " +
+                "structure defended by the opposing team known as the \"Ancient\", whilst defending their own.!",
+        modifier = modifier
+            .size(330.dp)
+            .offset((40).dp, (-10).dp)
+    )
+}
+@Composable
+fun DescriptionComment() {
+    Text(
+    text = "Review & Ratings",
+    color = TextColor,
+    modifier = Modifier
+        .fillMaxSize()
+        .offset(40.dp, (-100).dp)
+)}
+@Composable
+fun SliderVideo() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .offset(40.dp, (-130).dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.screen1),
+            contentDescription = null,
+            modifier = Modifier
+                .size(240.dp, 135.dp)
+                .clip(RoundedCornerShape(13.54.dp))
+        )
+        Image(
+            painter = painterResource(id = R.drawable.screen2),
+            contentDescription = null,
+            modifier = Modifier
+                .size(140.dp, 131.dp)
+                .clip(RoundedCornerShape(35.64.dp))
+
+        )
+    }
+}
+
+@Composable
+fun Comment() {
+    Row( modifier = Modifier
+        .fillMaxWidth()
+        .offset(40.dp, (-80).dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.person1),
+            contentDescription = null,
+            modifier = Modifier
+                .size(36.dp)
+                .clip(RoundedCornerShape(35.64.dp))
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+        ) {
+            Text(
+                text = "Auguste Conte",
+                color = TextColor,
+                modifier = Modifier.offset(10.dp, 0.dp)
+            )
+            Text(
+                text = "February 14, 2019",
+                color = TextColor,
+                modifier = Modifier.offset(10.dp, 0.dp)
+            )
+        }
+    }
+        Text(
+            text = "Once you start to learn its secrets, there’s a wild and exciting variety " +
+                    "of play here that’s unmatched, even by its peers." ,
+            color = TextColor,
+            modifier = Modifier.offset(40.dp, (-70).dp)
+                .size(330.dp, 100.dp)
+        )
+
+}
+@Composable
+fun ButtonInstall() {
+    Box(
+        modifier = Modifier
+            .offset(40.dp, (-50).dp)
+            .width(327.dp)
+            .height(64.dp)
+            .clip(RoundedCornerShape(13.54.dp))
+            .background(Yellow)
+    ) {
+        Text(
+            text = "Install",
+            color = Background,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .offset(0.dp, 20.dp)
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/example/app3/ui/theme/Color.kt
+++ app/src/main/java/com/example/app3/ui/theme/Color.kt
@@ -6,6 +6,9 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 val Yellow = Color(0xFFF4D144)
+val Background = Color(0xFF050B18)
+val TextColor = Color(0xFFA8ADB7)
+
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
